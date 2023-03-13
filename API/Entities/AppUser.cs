a184ce6fd71aa39e2d20542eb10f11b916cb0207
Surya Pandey <rajpandey2u@gmail.com>
2023-03-13 21:26:05 +0530

    13032023 changes commit

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,11 +1,28 @@
 namespace API.Entities
 {
-    public class AppUser
+    public class AppUser : AbstractAppUser, IAppUser
     {
         public int Id { get; set; }
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
 
+        public override void Display()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public abstract class AbstractAppUser
+    {
+        public abstract void Display();
+    }
+
+    interface IAppUser
+    {
+        void Display();
     }
 }
+
+
+
+
